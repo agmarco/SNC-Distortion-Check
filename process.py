8fde4d25dbef01a3fc8b3225d02dbda1f373a686
J. David Giese <johndgiese@gmail.com>
2016-09-30 22:58:27 -0400

    Initial implementation of combine dicome slices
    There are likely a still bugs in this implementation due to floating
    point errors.
    
    Also, the code can probably be simplified in a few places.

--- process.py
+++ process.py
@@ -1,5 +1,157 @@
 import numpy
 
 
-def combine_dicom_slices(slices):
-    return numpy.ones((2, 3))
+def combine_dicom_slices(slice_datasets):
+    '''
+    Given a list of pydicom datasets for a series, stitch them together into a
+    three-dimensional numpy array that is oriented with the "patient coordinate
+    system".  That is, the numpy array's dimensions will be oriented so that
+    the first index is along the x-dimension, the second along they
+    y-dimension, and the third along the z-dimension.
+
+    The numpy array will be cast to floating point values.
+
+    This function requires that the datasets fulfill these properties:
+
+    - Are all part of the same series (0020,000E)
+    - The set of slices provided has no internal "gaps"; note that missing
+      slices on the ends of the dataset are not detected
+    - The direction cosines for the rows and columns (0028,0037) are oriented
+      along the axes of the patient coordinate system
+    - The size of each slice and the spacing between the pixels in the slice
+      must be the same (0020,0032)
+    '''
+    if len(slice_datasets) == 0:
+        raise ValueError("Must provide at least one dataset")
+
+    _ensure_slices_form_a_uniform_grid(slice_datasets)
+
+    image_orientation = slice_datasets[0].ImageOrientationPatient
+    (row_axis, column_axis, slice_axis), (row_flip, column_flip) = _axes_mapping(image_orientation)
+
+    shape = [0, 0, 0]
+    shape[row_axis] = slice_datasets[0].Rows
+    shape[column_axis] = slice_datasets[0].Columns
+    shape[slice_axis] = len(slice_datasets)
+    data = numpy.empty(shape)
+
+    sorted_slice_datasets = sorted(slice_datasets, key=lambda d: d.ImagePositionPatient[slice_axis])
+    for c, dataset in enumerate(sorted_slice_datasets):
+        slices = [slice(None), slice(None), slice(None)]
+        slices[slice_axis] = c
+
+        slice_data = dataset.pixel_array[::row_flip, ::column_flip]
+
+        if row_axis > column_axis:
+            slice_data = slice_data.T
+
+        data[slices] = slice_data
+
+    return data
+
+
+def _validate_image_orientation(image_orientation):
+    '''
+    Ensure that the image orientation is supported
+
+    - The direction cosines have magnitudes of 1 (just in case)
+    - The direction cosines are perpendicular
+    - The direction cosines are oriented along the patient coordinate system's axes
+    '''
+    row_cosine, column_cosine = _extract_cosines(image_orientation)
+
+    if numpy.dot(row_cosine, column_cosine) != 0:
+        raise ValueError("Non-orthogonal direction cosines")
+
+    if numpy.linalg.norm(row_cosine) != 1.0:
+        raise ValueError("The row direction cosine's magnitude is not 1")
+
+    if numpy.linalg.norm(column_cosine) != 1.0:
+        raise ValueError("The column direction cosine's magnitude is not 1")
+
+    if numpy.max(numpy.abs(row_cosine)) != 1.0 or numpy.max(numpy.abs(column_cosine)) != 1.0:
+        message = "Currently we only support DICOM files where the images are oriented along the patient coordinate system."
+        raise ValueError(message)
+
+
+def _axes_mapping(image_orientation):
+    '''
+    The DICOM pixel data can be oriented in a number of different ways.  We
+    need to simplify all of the variations out, and a key part of doing this is
+    mapping the pixel data axes into patient coordinate system axes.
+
+    The return values are a 3-tuple representing the mapping for the row, column,
+    and slice axis respectively, and a 2-tuple indicating if the row or column
+    axis needs to be flipped (the slice axis will never need to be flipped
+    because we sort it according to the ImagePositionPatient which is already
+    inside the patient's coordinate system.
+
+    Each value maps onto patient coordinate system axis.  A
+    value of 0 maps to the x-axis, 1 the y-axis, and 2 the z-axis.
+
+    NOTE we assume that the cosines are oriented along the main patient axes
+    '''
+    row_cosine, column_cosine = _extract_cosines(image_orientation)
+
+    row_axis = numpy.argmax(numpy.abs(row_cosine))
+    column_axis = numpy.argmax(numpy.abs(column_cosine))
+
+    slice_axis_cosine = numpy.cross(row_cosine, column_cosine)
+    slice_axis = numpy.argmax(numpy.abs(slice_axis_cosine))
+
+    row_flip = numpy.sign(row_cosine[row_axis])
+    column_flip = numpy.sign(column_cosine[column_axis])
+
+    return (row_axis, column_axis, slice_axis), (row_flip, column_flip)
+
+
+def _extract_cosines(image_orientation):
+    row_cosine = numpy.array(image_orientation[:3])
+    column_cosine = numpy.array(image_orientation[3:])
+    return row_cosine, column_cosine
+
+
+def _ensure_slices_form_a_uniform_grid(slice_datasets):
+    '''
+    Perform various data checks to ensure that the list of slices form a
+    evenly-spaced grid of data.
+
+    Some of these checks are probably not required if the data follows the
+    DICOM specification, however it seems pertinent to check anyway.
+    '''
+    image_orientation = slice_datasets[0].ImageOrientationPatient
+    _validate_image_orientation(image_orientation)
+
+    (row_axis, column_axis, slice_axis), _ = _axes_mapping(image_orientation)
+
+    series_instance_uid = slice_datasets[0].SeriesInstanceUID
+    pixel_spacing = slice_datasets[0].PixelSpacing
+    row_position = slice_datasets[0].ImagePositionPatient[row_axis]
+    column_position = slice_datasets[0].ImagePositionPatient[column_axis]
+    rows = slice_datasets[0].Rows
+    columns = slice_datasets[0].Columns
+
+    slice_positions = []
+
+    for dataset in slice_datasets:
+        if dataset.SeriesInstanceUID != series_instance_uid:
+            raise ValueError('It seems there are slices from different Series')
+        if dataset.PixelSpacing != pixel_spacing:
+            raise ValueError('All slices must have the same pixel spacing')
+        if dataset.ImageOrientationPatient != image_orientation:
+            raise ValueError('All slices must have the same image orientation')
+        if dataset.ImagePositionPatient[row_axis] != row_position:
+            raise ValueError('All slices must line up along the row axis')
+        if dataset.ImagePositionPatient[column_axis] != column_position:
+            raise ValueError('All slices must line up along the column axis')
+        if dataset.Rows != rows or dataset.Columns != columns:
+            raise ValueError('All slices must be the same size')
+        slice_positions.append(dataset.ImagePositionPatient[slice_axis])
+
+    _check_for_missing_slices(slice_positions)
+
+
+def _check_for_missing_slices(slice_positions):
+    slice_positions_diffs = numpy.diff(sorted(slice_positions))
+    if any(slice_positions_diffs - slice_positions_diffs[0] != 0):
+        raise ValueError("Missing slices")
--- process_test.py
+++ process_test.py
@@ -1,51 +1,233 @@
+from unittest import mock
+import math
+
 import pytest
 import numpy
-import dicom
+from numpy.random import rand
 
 from process import combine_dicom_slices
 
 
-def generate_test_slice(z, nx=10, ny=11):
-    slice_data_set = dicom.dataset.Dataset()
-    slice_data_set.pixel_array = numpy.random.rand(nx, ny)
-    slice_data_set.ImagePositionPatient = [-nx/2.0, -ny/2.0, z]
-    return slice_data_set
+# direction cosines
+x_cos = (1, 0, 0)
+y_cos = (0, 1, 0)
+z_cos = (0, 0, 1)
+negative_x_cos = (-1, 0, 0)
+negative_y_cos = (0, -1, 0)
+negative_z_cos = (0, 0, -1)
+
+arbitrary_shape = (10, 11)
+
+
+def generate_mock_slice(pixel_array, slice_position, row_cosine, column_cosine):
+    '''
+    Build a minimal DICOM dataset representing a dataslice at a particular
+    slice location.  The `slice_position` is the coordinate value along the
+    remaining unused axis (i.e. the axis perpendicular to the direction
+    cosines).
+    '''
+    na, nb = pixel_array.shape
+
+    dataset = mock.Mock()
+    dataset.pixel_array = pixel_array
+
+    dataset.SeriesInstanceUID = 'arbitrary uid'
+    dataset.PixelSpacing = [1.0, 1.0]
+    dataset.Rows = na
+    dataset.Columns = nb
+
+    # assume that the images are centered on the remaining unused axis
+    a_component = [-na/2.0*c for c in row_cosine]
+    b_component = [-nb/2.0*c for c in column_cosine]
+    c_component = [(slice_position if c == 0 and cc == 0 else 0) for c, cc in zip(row_cosine, column_cosine)]
+    patient_position = [a + b + c for a, b, c in zip(a_component, b_component, c_component)]
+
+    dataset.ImagePositionPatient = patient_position
+
+    dataset.ImageOrientationPatient = list(row_cosine) + list(column_cosine)
+    return dataset
 
 
 @pytest.fixture
-def slices():
+def axial_slices():
     return [
-        generate_test_slice(0),
-        generate_test_slice(1),
-        generate_test_slice(2),
-        generate_test_slice(3),
+        generate_mock_slice(rand(*arbitrary_shape), 0, x_cos, y_cos),
+        generate_mock_slice(rand(*arbitrary_shape), 1, x_cos, y_cos),
+        generate_mock_slice(rand(*arbitrary_shape), 2, x_cos, y_cos),
+        generate_mock_slice(rand(*arbitrary_shape), 3, x_cos, y_cos),
     ]
 
 
-def test_combine_dicom_slices_simple_case(slices):
-    combined = combine_dicom_slices(slices[0:2])
-    manually_combined = numpy.dstack((slices[0].pixel_array, slices[1].pixel_array))
+def test_combine_dicom_slices_axial(axial_slices):
+    '''
+    Axial slices cut perpendicular to the spinalchord.  Hence the row-cosine is
+    in the x-direction in the patient coordinate system, and the column-cosine
+    is in the y-direction in the patient coordinate system.
+    '''
+    combined = combine_dicom_slices(axial_slices[0:2])
+    manually_combined = numpy.dstack((axial_slices[0].pixel_array, axial_slices[1].pixel_array))
     assert numpy.array_equal(combined, manually_combined)
 
 
-def test_combine_dicom_slices_is_robust_to_ordering(slices):
-    assert numpy.array_equal(
-        combine_dicom_slices([slices[0], slices[1], slices[2]]),
-        combine_dicom_slices([slices[1], slices[0], slices[2]])
-    )
+def test_combine_dicom_slices_rows_and_cols_swapped():
+    '''
+    The direction cosines can be swapped.  In this case, these are still
+    axial slices, however the slice-images have been transposed.
+    '''
+    slice_0_data = rand(*arbitrary_shape)
+    slice_1_data = rand(*arbitrary_shape)
+    slices = [
+        generate_mock_slice(slice_0_data, 0, y_cos, x_cos),
+        generate_mock_slice(slice_1_data, 1, y_cos, x_cos),
+    ]
 
-    assert numpy.array_equal(
-        combine_dicom_slices([slices[0], slices[1], slices[2]]),
-        combine_dicom_slices([slices[2], slices[0], slices[1]])
-    )
+    combined = combine_dicom_slices(slices)
+    manually_combined = numpy.dstack((slice_0_data.T, slice_1_data.T))
+    assert numpy.array_equal(combined, manually_combined)
 
 
-def test_combine_dicom_slices_missing_middle_slice(slices):
+def test_combine_dicom_slices_row_direction_inverted():
+    '''
+    The direction cosines can also be negative.  In this case, these are still
+    axial slices, however the slice-images have been reflected across one of
+    the image axes.
+    '''
+    slice_0_data = rand(*arbitrary_shape)
+    slice_1_data = rand(*arbitrary_shape)
+    slices = [
+        generate_mock_slice(slice_0_data, 0, negative_x_cos, y_cos),
+        generate_mock_slice(slice_1_data, 1, negative_x_cos, y_cos),
+    ]
+
+    combined = combine_dicom_slices(slices)
+    manually_combined = numpy.dstack((slice_0_data[::-1, :], slice_1_data[::-1, :]))
+    assert numpy.array_equal(combined, manually_combined)
+
+
+def test_combine_dicom_slices_sagital():
+    '''
+    Sagittal slices cut-sideways through the person.  E.g. a sagital slice will
+    show the profile of a person.
+
+    Typically, the person will be "looking" to the left-side of the image,
+    hence the "row-cosine" points in the positive y-direction in the patient
+    coordinate system, and the "column-cosine" points in the negative
+    z-direction in the patient coordinate system.
+    '''
+    slice_0_data = rand(*arbitrary_shape)
+    slice_1_data = rand(*arbitrary_shape)
+    slices = [
+        generate_mock_slice(slice_0_data, 0, y_cos, negative_z_cos),
+        generate_mock_slice(slice_1_data, 1, y_cos, negative_z_cos),
+    ]
+
+    combined = combine_dicom_slices(slices)
+
+    manually_combined = numpy.empty([2, arbitrary_shape[0], arbitrary_shape[1]])
+    manually_combined[0, :, :] = slice_0_data[:, ::-1]
+    manually_combined[1, :, :] = slice_1_data[:, ::-1]
+
+    assert numpy.array_equal(combined, manually_combined)
+
+
+def test_combine_dicom_slices_coronal():
+    '''
+    Coronal (or frontal) slices, in humans, cuts through both ears, hands and feet.  In this case, we
+    have the row direction-cosine pointing in the positive x-direction, and the
+    column direction-cosine pointing in the negative z-direction.
+    '''
+    slice_0_data = rand(*arbitrary_shape)
+    slice_1_data = rand(*arbitrary_shape)
+    slices = [
+        generate_mock_slice(slice_0_data, 0, x_cos, negative_z_cos),
+        generate_mock_slice(slice_1_data, 1, x_cos, negative_z_cos),
+    ]
+
+    combined = combine_dicom_slices(slices)
+
+    manually_combined = numpy.empty([arbitrary_shape[0], 2, arbitrary_shape[1]])
+    manually_combined[:, 0, :] = slice_0_data[:, ::-1]
+    manually_combined[:, 1, :] = slice_1_data[:, ::-1]
+
+    assert numpy.array_equal(combined, manually_combined)
+
+
+def test_combine_dicom_slices_non_orthogonal_direction_cosines(axial_slices):
+    '''
+    We assume that the direction cosines are perpendicular to each other.
+    '''
+    one_degree = 1*math.pi/180
+    non_orthogonal_orientation = [1, 0, 0, math.sin(one_degree), math.cos(one_degree), 0]
+    for dataset in axial_slices:
+        dataset.ImageOrientationPatient = non_orthogonal_orientation
+
     with pytest.raises(ValueError):
-        combine_dicom_slices([slices[0], slices[2], slices[3]])
+        combine_dicom_slices(axial_slices)
+
+
+def test_combine_dicom_slices_non_standard_direction_cosines(axial_slices):
+    '''
+    Currently was assume that the direction cosines are oriented perpendicular
+    to the axes of the patient coordinate system.
+    '''
+    one_degree = 1*math.pi/180
+    non_standard_orientation = [-math.cos(one_degree), math.sin(one_degree), 0, \
+                                   math.sin(one_degree), math.cos(one_degree), 0]
+    for dataset in axial_slices:
+        dataset.ImageOrientationPatient = non_standard_orientation
+
+    with pytest.raises(ValueError):
+        combine_dicom_slices(axial_slices)
+
+
+def test_combine_dicom_slices_casts_to_float(axial_slices):
+    '''
+    The typically integer DICOM pixel data should be cast to a float.
+    '''
+    assert combine_dicom_slices(axial_slices).dtype == numpy.dtype('float64')
+
+
+def test_combine_dicom_slices_is_robust_to_ordering(axial_slices):
+    '''
+    The DICOM slices should be able to be passed in in any order, and they
+    should be recombined appropriately.
+    '''
+    assert numpy.array_equal(
+        combine_dicom_slices([axial_slices[0], axial_slices[1], axial_slices[2]]),
+        combine_dicom_slices([axial_slices[1], axial_slices[0], axial_slices[2]])
+    )
+
+    assert numpy.array_equal(
+        combine_dicom_slices([axial_slices[0], axial_slices[1], axial_slices[2]]),
+        combine_dicom_slices([axial_slices[2], axial_slices[0], axial_slices[1]])
+    )
+
+
+def test_combine_dicom_slices_missing_middle_slice(axial_slices):
+    '''
+    All slices must be present.  Slice position is determined using the
+    ImagePositionPatient (0020,0032) tag.
+    '''
+    with pytest.raises(ValueError):
+        combine_dicom_slices([axial_slices[0], axial_slices[2], axial_slices[3]])
+
+
+def test_combine_dicom_slices_slice_from_different_series(axial_slices):
+    '''
+    As a sanity check, slices that don't come from the same DICOM series should
+    be rejected.
+    '''
+    axial_slices[2].SeriesInstanceUID += 'Ooops'
+    with pytest.raises(ValueError):
+        combine_dicom_slices(axial_slices)
 
 
 @pytest.mark.xfail(reason='Not sure how to detect this in DICOM')
-def test_combine_dicom_slices_missing_end_slice(slices):
+def test_combine_dicom_slices_missing_end_slice(axial_slices):
+    '''
+    Ideally, we would detect missing edge slices, however given that we don't
+    know any way to determine the number of slices are in a DICOM series, this
+    seems impossible.
+    '''
     with pytest.raises(ValueError):
-        combine_dicom_slices([slices[0], slices[1], slices[2]])
+        combine_dicom_slices([axial_slices[0], axial_slices[1], axial_slices[2]])
