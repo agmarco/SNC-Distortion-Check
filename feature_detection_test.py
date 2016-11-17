81c0491c017904928d58efeff90ecfa1fdeff3e4
J. David Giese <johndgiese@gmail.com>
2016-11-16 22:03:27 -0500

    Fix bugs in fearture detection test suite

--- feature_detection_test.py
+++ feature_detection_test.py
@@ -4,37 +4,53 @@ import numpy as np
 import scipy.io
 import dicom
 
-from algorithm_test_runner.suite import Suite
+from hdatt.suite import Suite
 from feature_detection import detect_features
 from dicom_import import combine_slices
+from points import categorize
 
 
 class FeatureDetectionSuite(Suite):
-    alias = 'feature-detection'
+    id = 'feature-detection'
 
     def collect(self):
         return {
             'mri-001-axial': {
-                'images': './data/mri-001-axial',
+                'images': './data/dicom/mri-001-axial',
                 'golden': './data/points/mri-001-axial-golden.mat',
             }
         }
 
-    def run(self, case_id, case_input):
+    def run(self, case_input):
+        metrics = {}
         context = {}
 
-        input_dicom_filenames = os.listdir(case_input['images'])
+        image_dir = os.path.abspath(case_input['images'])
+        input_dicom_filenames = [os.path.join(image_dir, p) for p in os.listdir(image_dir)]
         dicom_datasets = [dicom.read_file(f) for f in input_dicom_filenames]
         voxels, ijk_to_patient_xyz_transform = combine_slices(dicom_datasets)
+        points = detect_features(voxels, ijk_to_patient_xyz_transform)
 
-        points_in_patient_xyz = detect_features(voxels, ijk_to_patient_xyz_transform)
-        context['points_in_patient_xyz'] = points_in_patient_xyz
+        golden_points = scipy.io.loadmat(case_input['golden'])['points']
+        FN_A, TP_A, TP_B, FP_B = categorize(golden_points, points, lambda bmag: 7.5)
 
-        # TODO: find all points that are matched w/in a certain distance
+        context['FN_A'] = FN_A
+        context['TP_A'] = TP_A
+        context['TP_B'] = TP_B
+        context['FP_B'] = FP_B
+
+        metrics['percent_false_negative'] = FN_A.shape[1]/(FN_A.shape[1] + TP_A.shape[1])*100
+        metrics['percent_false_positive'] = FP_B.shape[1]/(FP_B.shape[1] + TP_B.shape[1])*100
+
+        mean_matched_displacement = np.average(TP_A - TP_B, axis=1)
+        metrics['mean_matched_distance'] = np.linalg.norm(mean_matched_displacement)
+
+        random_errors = TP_A - TP_B - mean_matched_displacement.reshape(3, 1)
+        metrics['mean_matched_random_distance'] = np.average(np.linalg.norm(random_errors))
 
         return metrics, context
 
-    def verify(self, old, new):
+    def verify(self, old_metrics, new_metrics):
         percent_change = abs(old - new)/old*100.0
         max_allowed_percent_change = 2.0
         passed = percent_change < max_allowed_percent_change
