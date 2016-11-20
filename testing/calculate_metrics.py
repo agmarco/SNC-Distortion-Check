cb10a90eb1c509ea35dfd70c71bab19693d79967
Yujan Shrestha <yshrestha@innolitics.com>
2016-11-20 16:08:14 -0600

    Add average and random error calculation to metrics.

--- testing/calculate_metrics.py
+++ testing/calculate_metrics.py
@@ -6,13 +6,21 @@ import scipy.io
 from points import categorize
 
 
-def calc_metrics(known_points_xyz, detected_points_xyz):
-    FN_A, TP_A, TP_B, FP_B = categorize(known_points_xyz, detected_points_xyz, lambda x: 5)
-    average_error = np.average(np.linalg.norm((TP_A - TP_B).T, axis=1))
-    FPF = len(FP_B.T) / len(known_points_xyz.T)
-    FNF = len(FN_A.T) / len(known_points_xyz.T)
-    TPF = len(TP_A.T) / len(known_points_xyz.T)
-    return average_error, TPF, FPF, FNF
+def calc_metrics(FN_A, TP_A, TP_B, FP_B):
+    num_points_a = len(FN_A.T) + len(TP_A.T)
+    error_vec = (TP_A - TP_B).T
+    average_error_vec = np.average(error_vec, axis=0)
+    error_vec_norms = np.linalg.norm(error_vec, axis=1)
+    average_error = np.average(error_vec_norms)
+
+    random_error_vec = error_vec - average_error_vec
+    random_error_norms = np.linalg.norm(random_error_vec, axis=1)
+    random_error_average = np.average(random_error_norms)
+
+    FPF = len(FP_B.T) / num_points_a
+    FNF = len(FN_A.T) / num_points_a
+    TPF = len(TP_A.T) / num_points_a
+    return average_error, random_error_average, TPF, FPF, FNF
 
 
 if __name__ == '__main__':
@@ -22,5 +30,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     known_points_xyz = scipy.io.loadmat(args.known_points)['points']
     detected_points_xyz = scipy.io.loadmat(args.detected_points)['points']
-    average_error, TPF, FPF, FNF = calc_metrics(known_points_xyz, detected_points_xyz)
-    print(average_error, TPF, FPF, FNF)
+    FN_A, TP_A, TP_B, FP_B = categorize(known_points_xyz, detected_points_xyz, lambda x: 5)
+    print(calc_metrics(FN_A, TP_A, TP_B, FP_B))
