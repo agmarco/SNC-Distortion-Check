76f6a07cb926bbe3aa295b58bc65d93bf3d0e505
J. David Giese <johndgiese@gmail.com>
2016-12-26 04:32:55 -0500

    Remove TODO that was a deadend

--- registration.py
+++ registration.py
@@ -62,7 +62,6 @@ def build_f(A, B, g, rho):
 
         summation = 0.0
         for a_s in A_S.T:
-            # TODO: make this use the KDTree for speed
             b_to_a_s_distances_squared = np.sum((B_consideration_order - a_s.reshape((3, 1)))**2, axis=0)
             closest_b_indice = np.argmin(b_to_a_s_distances_squared) 
             b_min_to_a_s = sqrt(b_to_a_s_distances_squared[closest_b_indice])
