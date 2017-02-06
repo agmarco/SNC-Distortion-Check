c24a93dffba8fa5056de0c2bea4717f9b5147db4
Yujan Shrestha <yshrestha@innolitics.com>
2017-02-06 02:38:48 -0600

    start using real data

--- keras-play.py
+++ keras-play.py
@@ -1,12 +1,15 @@
-'''Transfer learning toy example:
-1- Train a simple convnet on the MNIST dataset the first 5 digits [0..4].
-2- Freeze convolutional layers and fine-tune dense layers
-   for the classification of digits [5..9].
-Run on GPU: THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python mnist_transfer_cnn.py
-Get to 99.8% test accuracy after 5 epochs
-for the first five digits classifier
-and 99.2% for the last five digits after transfer + fine-tuning.
-'''
+
+from tensorflow.python.client import device_lib
+
+from affine import apply_affine
+
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+print(get_available_gpus())
+
 import random
 
 import math
@@ -15,8 +18,10 @@ import datetime
 import itertools
 
 from scipy.ndimage.interpolation import geometric_transform
+from scipy.signal import fftconvolve, gaussian
 from tensorflow.contrib.layers.python.layers.layers import one_hot_encoding
 
+import file_io
 from keras.layers.convolutional import Convolution3D
 from keras.layers.core import SpatialDropout3D
 from keras.layers.normalization import BatchNormalization
@@ -24,7 +29,7 @@ from keras.layers.pooling import MaxPooling3D
 from keras.preprocessing.text import one_hot
 from kernels import cylindrical_grid_intersection, sphere
 from slicer import show_slices
-from testing.distort_voxel import affine_transform_func, progress_indicator, to_xyz, chain_transformers
+from testing.distort_voxel import affine_transform_func, chain_transformers, deform_func, affine_point
 from utils import decimate
 
 np.random.seed(1337)  # for reproducibility
@@ -36,13 +41,54 @@ from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 cube_size = 13
-center = math.floor(cube_size/2)
+cube_size_half = math.floor(cube_size / 2)
 input_shape = (cube_size,cube_size,cube_size,1)
-num_filters = 9
+num_filters = 24
 kernel_shape = (3,3,3)
 maxpool_size = pool_size=(2, 2, 2)
 nb_classes = 2
 batch_size = None
+
+def real_intersection_generator():
+    voxel_data = file_io.load_voxels('tmp/xxx_ct_1540_ST075-120kVp-25mA-voxels.mat')
+    voxels = voxel_data['voxels']
+    ijk_to_xyz = voxel_data['ijk_to_patient_xyz_transform']
+    xyz_to_ijk = np.linalg.inv(ijk_to_xyz)
+    golden_points = file_io.load_points('data/points/012_ct_1540_ST075-120kVp-25mA-golden.mat')['points']
+    point_xyz = random.choice(golden_points.T)
+    point_ijk = affine_point(xyz_to_ijk, point_xyz.T)
+    point_ijk = np.round(point_ijk)
+    i, j, k = point_ijk.astype(int)
+    voxel_window = voxels[i-cube_size_half:i+cube_size_half+1, j-cube_size_half:j+cube_size_half+1, k-cube_size_half:k+cube_size_half+1]
+    return np.expand_dims(voxel_window, axis=3)
+
+def real_confusion_generator():
+    voxel_data = file_io.load_voxels('tmp/xxx_ct_1540_ST075-120kVp-25mA-voxels.mat')
+    voxels = voxel_data['voxels']
+    ijk_to_xyz = voxel_data['ijk_to_patient_xyz_transform']
+    xyz_to_ijk = np.linalg.inv(ijk_to_xyz)
+    golden_points = file_io.load_points('data/points/012_ct_1540_ST075-120kVp-25mA-golden.mat')['points']
+    point_xyz = random.choice(golden_points.T)
+    point_ijk = affine_point(xyz_to_ijk, point_xyz.T)
+    point_ijk = point_ijk + random.choice([1, -1]) * (np.random.sample(3)*10 + 1)
+    point_ijk = np.round(point_ijk)
+    i, j, k = point_ijk.astype(int)
+    voxel_window = voxels[i-cube_size_half:i+cube_size_half+1, j-cube_size_half:j+cube_size_half+1, k-cube_size_half:k+cube_size_half+1]
+    return np.expand_dims(voxel_window, axis=3)
+
+
+def distort_voxels(voxels, distortion_factor):
+    to_pad = 5 # so that transforms will be valid on edges.
+    center = math.floor((cube_size+2*to_pad)/2)
+    voxels = voxels.squeeze()
+    voxels = np.pad(voxels, ((to_pad, to_pad), (to_pad, to_pad), (to_pad, to_pad)), 'edge')
+    to_xyz_func, from_xyz_func = affine_transform_func(-center, -center, -center, 0, 0, 0)
+    distort_func, undistort_func = deform_func(distortion_factor)
+    distort_func = chain_transformers([to_xyz_func, undistort_func, from_xyz_func])
+    voxels_distorted = geometric_transform(voxels, distort_func)
+    output = np.expand_dims(voxels_distorted[to_pad:-to_pad, to_pad:-to_pad, to_pad:-to_pad], axis=3)
+    assert output.shape == input_shape
+    return output
 
 
 def generate_kernel(radius=3, spacing=cube_size):
@@ -56,10 +102,12 @@ def generate_kernel(radius=3, spacing=cube_size):
     return kernel
 
 intersection_bank = [generate_kernel(radius=i) for i in np.linspace(1, 2.5, 10)]
+# intersection_bank.extend([distort_voxels(voxels, 3e-2) for voxels in intersection_bank])
+
 
 
 def generate_sphere():
-    return np.expand_dims(sphere(pixel_spacing=(1,1,1), radius=center), axis=3)
+    return np.expand_dims(sphere(pixel_spacing=(1,1,1), radius=cube_size_half), axis=3)
 
 def generate_cylinder():
     intersection = random.choice(intersection_bank)
@@ -87,6 +135,14 @@ def transform_voxels(voxels, x, y, z, theta, pi, xhi):
     assert output.shape == input_shape
     return output
 
+# def gaussian_blurrer(voxels):
+#     kernel = np.outer(gaussian(3, 1), gaussian(3, 1), gaussian(3, 1))
+#     import pydevd;pydevd.settrace('localhost', port=63421, stdoutToServer=True, stderrToServer=True)
+#     return fftconvolve(voxels, kernel, mode='same')
+#
+# show_slices(gaussian_blurrer(intersection_bank[0]).squeeze())
+#
+
 def rotation_augmentor(voxels):
     '''rotates the voxels by a random amount in all 3 axis.'''
     max_rot = 8
@@ -94,8 +150,8 @@ def rotation_augmentor(voxels):
 
 def rotation_translation_augmentor(voxels):
     '''rotates the voxels by a random amount in all 3 axis.'''
-    max_rot = 8
-    min_trans = 1
+    max_rot = 4
+    min_trans = 0.5
     max_trans = 5
     trans_mag = max_trans-min_trans
     return transform_voxels(voxels, np.random.sample()*trans_mag+min_trans,  np.random.sample()*trans_mag+min_trans,  np.random.sample()*trans_mag+min_trans,
@@ -111,18 +167,20 @@ intersection_augmenters = [
     lambda X: X+np.random.random_sample(),
     lambda X: X*np.random.normal(0, np.random.sample()*0.1, X.shape),
     lambda X: np.max(X) - X,
-    rotation_augmentor
+    rotation_augmentor,
+    lambda X: X
 ]
 
 confusion_generators = [
-    lambda: np.random.uniform(0, 1, input_shape),
-    lambda: np.random.normal(0, np.random.sample(), input_shape),
-    lambda: np.ones(input_shape) * np.random.sample(),
-    generate_sphere,
-    generate_cylinder,
-    generate_shifted_intersection,
-    generate_shifted_rotated_intersection,
-    generate_shifted_cylinder
+    # # lambda: np.random.uniform(0, 1, input_shape),
+    #  lambda: np.random.normal(0, np.random.sample(), input_shape),
+    # # lambda: np.ones(input_shape) * np.random.sample(),
+    # generate_sphere,
+    # generate_cylinder,
+    # generate_shifted_intersection,
+    # generate_shifted_rotated_intersection,
+    # generate_shifted_cylinder,
+    real_confusion_generator
 ]
 
 def training_generator():
@@ -138,7 +196,7 @@ def training_generator():
 
 def intersection_generator():
     while True:
-        kernel = random.choice(intersection_bank)
+        kernel = real_intersection_generator()
         augmenter = random.choice(intersection_augmenters)
         kernel = augmenter(kernel)
         yield kernel
@@ -167,11 +225,11 @@ def conv_unit(num_filters, num_convs):
         Convolution3D(num_filters, *kernel_shape, border_mode='same'),
         Activation('relu'),
         BatchNormalization(),
-        # Dropout(0.2)
+        Dropout(0.2)
     ] for _ in range(num_convs)), [MaxPooling3D(pool_size)]))
 
 
-feature_layers = conv_unit(num_filters, 2) + conv_unit(num_filters*2, 2) + conv_unit(num_filters*4, 2)
+feature_layers = conv_unit(num_filters, 2) + conv_unit(num_filters*2, 3) + conv_unit(num_filters*4, 3) + conv_unit(num_filters*4, 3)
 feature_layers[0] = Convolution3D(num_filters, *kernel_shape, border_mode='same', input_shape=input_shape)
 # remove that last maxpool
 feature_layers = feature_layers[:-1]
@@ -179,11 +237,13 @@ feature_layers = feature_layers[:-1]
 
 classification_layers = [
     Flatten(),
-    Dense(128),
-    # BatchNormalization(),
-    Dense(128),
-    # BatchNormalization(),
-    # Dropout(0.2),
+    Dense(256),
+    Activation('relu'),
+    BatchNormalization(),
+    Dropout(0.2),
+    Dense(256),
+    Activation('relu'),
+    BatchNormalization(),
     Dense(nb_classes),
     Activation('softmax')
 ]
@@ -193,7 +253,7 @@ model = Sequential(feature_layers + classification_layers)
 model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-batch_gen = batch_generator(5, training_generator())
-model.fit_generator(batch_gen, samples_per_epoch=320, nb_epoch=10, verbose=1)
+batch_gen = batch_generator(32, training_generator())
+model.fit_generator(batch_gen, samples_per_epoch=320, nb_epoch=1000, verbose=1)
 model.save('keras-test.h5')
 # import pydevd;pydevd.settrace('localhost', port=63421, stdoutToServer=True, stderrToServer=True)
