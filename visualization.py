bab791d496216664c54f122e61f942be9fc9fddf
J. David Giese <johndgiese@gmail.com>
2017-02-11 10:55:28 -0500

    Increase contrast between scatter plot colors

--- visualization.py
+++ visualization.py
@@ -7,7 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def scatter3(label_to_points):
-    colors = itertools.cycle(["c", "g", "r", "y", "k"])
+    colors = itertools.cycle(["c", "r", "g", "y", "k"])
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
