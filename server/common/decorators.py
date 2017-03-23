6034b132f64fb1e39f768b6a9bbcc1b5e1be181f
Zach Waggoner <zach.waggoner1@gmail.com>
2017-03-23 18:52:35 -0500

    Check that validate_institution is used with an object that has an 'institution' property.

--- server/common/decorators.py
+++ server/common/decorators.py
@@ -26,6 +26,8 @@ def validate_institution(model_class=None, pk_url_kwarg='pk'):
                     obj = instance.get_object()
                 else:
                     raise Exception("You must either specify the model_class, or implement the get_object method.")
+                if not hasattr(obj, 'institution'):
+                    raise Exception(f"The property 'institution' was not found on the object {obj}.")
                 if obj.institution != request.user.institution:
                     raise PermissionDenied
                 return old_dispatch(instance, request, *args, **kwargs)
@@ -37,6 +39,8 @@ def validate_institution(model_class=None, pk_url_kwarg='pk'):
             @wraps(view)
             def wrapper(request, *args, **kwargs):
                 obj = get_object_or_404(model_class, pk=kwargs[pk_url_kwarg])
+                if not hasattr(obj, 'institution'):
+                    raise Exception(f"The property 'institution' was not found on the object {obj}.")
                 if obj.institution != request.user.institution:
                     raise PermissionDenied
                 return view(request, *args, **kwargs)
