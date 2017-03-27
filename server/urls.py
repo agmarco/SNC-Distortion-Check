b4e669d2ccad920ba98e4847c843a310333e253b
Zach Waggoner <zach.waggoner1@gmail.com>
2017-03-27 14:44:09 -0500

    Added header/footer.

--- server/urls.py
+++ server/urls.py
@@ -17,13 +17,14 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 admin.site.site_header = 'CIRS Distortion Check Admin'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': reverse_lazy('login')}, name='logout'),
     url(r'^', include('server.common.urls')),
 ]
 
