d1477b5ffd49e268330389254182d9bdd1766733
Zach Waggoner <zach.waggoner1@gmail.com>
2017-04-13 14:13:15 -0500

    Attempt to get heroku working

---
+++ webpack.config.base.babel.js
@@ -0,0 +1,61 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+export default (env) => ({
+    entry: {
+        vendor: [
+            'babel-polyfill',
+            'react-hot-loader/patch',
+            'react-hot-loader',
+            'react',
+            'react-dom',
+        ],
+        landing: [path.join(__dirname, 'client/src/landing/app.tsx')],
+        machine_sequences: [path.join(__dirname, 'client/src/machine_sequences/app.tsx')],
+        machine_sequence_detail: [path.join(__dirname, 'client/src/machine_sequence_detail/app.tsx')],
+        create_phantom: [path.join(__dirname, 'client/src/create_phantom/app.tsx')],
+        upload_scan: [path.join(__dirname, 'client/src/upload_scan/app.tsx')],
+
+        // CSS only:
+        base: [path.join(__dirname, 'client/src/base/app.scss')],
+        login: [path.join(__dirname, 'client/src/login/app.scss')],
+        configuration: [path.join(__dirname, 'client/src/configuration/app.scss')],
+        update_phantom: [path.join(__dirname, 'client/src/update_phantom/app.scss')],
+    },
+
+    output: {
+        path: path.join(__dirname, 'client/dist'),
+    },
+
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader'],
+            }, {
+
+                // TODO HMR CSS
+                // TODO don't include the base CSS in the other chunks
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader'],
+                }),
+            },
+        ],
+    },
+
+    resolve: {
+        modules: [
+            path.join(__dirname, 'client/src'),
+            path.join(__dirname, 'node_modules'),
+        ],
+        extensions: ['.webpack.js', '.web.js', '.js', '.jsx', '.ts', '.tsx'],
+    },
+});
