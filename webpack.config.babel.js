bbd456f228bbbf10a667128c162a317e5f3eb28d
Zach Waggoner <zach.waggoner1@gmail.com>
2017-04-17 21:04:09 -0500

    Fix formatting

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,7 +2,7 @@ export default (env) => {
     if (['dev', 'hot'].includes(env)) {
         return require('./webpack.config.dev.babel').default(env);
     } else if (env === 'test') {
-        return require('./webpack.config.test.babel').default(env);
+        return require('./webpack.config.test.babel').default;
     } else if (env === 'prod') {
         return require('./webpack.config.prod.babel').default(env);
     }
