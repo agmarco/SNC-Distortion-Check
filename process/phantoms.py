64247e8dae3c10fdd95efec70ccb731b940ba959
J. David Giese <johndgiese@gmail.com>
2017-05-01 17:37:15 -0400

    Add todo concerning phantoms.py

--- process/phantoms.py
+++ process/phantoms.py
@@ -1,3 +1,15 @@
+# TODO: Deduplicate this data
+
+# Some of the information in this file is duplicated in
+#
+#   server.common.models.PhantmoModel
+#
+# We should probably only have one source of truth, and probably it should be
+# the database.  We will likely include Keras models for each phantom model,
+# and these model files are too large to store directly in the repository
+# long-term; hence, long term we probably want to keep phantom models in the
+# database.
+
 paramaters = {
     '603A': {
         'points_file': 'data/points/603A.mat',
