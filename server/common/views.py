44af0ffe345ce9671f2bcd4dcfd1d938e6951270
Zach Waggoner <zach.waggoner1@gmail.com>
2017-05-03 16:20:50 -0500

    Fix circular import

--- server/common/views.py
+++ server/common/views.py
@@ -1,3 +1,7 @@
+import os
+import io
+import uuid
+import zipfile
 import logging
 from datetime import datetime
 
@@ -10,9 +14,13 @@ from django.utils.functional import cached_property
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, ModelFormMixin, FormView
 from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
 from rest_framework.renderers import JSONRenderer
 
+import scipy.io
+
 from process import dicom_import
+from process.file_io import save_voxels
 from . import models
 from . import serializers
 from .tasks import process_scan, process_ct_upload
@@ -465,5 +473,63 @@ def gold_standard_csv(request, phantom_pk=None, gold_standard_pk=None):
 @validate_institution(model_class=models.Scan)
 def raw_data(request, pk=None):
     scan = get_object_or_404(models.Scan, pk=pk)
-    zipfile = models.dump_raw_data(scan)
+    zipfile = dump_raw_data(scan)
     return ZipResponse(zipfile, filename=f'raw_data.zip')
+
+
+def dump_raw_data(scan):
+    voxels_path = os.path.join(settings.BASE_DIR, f'tmp/{uuid.uuid4()}.mat')
+    voxels_data = {
+        'phantom_name': scan.phantom.model.model_number,
+        'modality': 'mri',
+        'voxels': scan.dicom_series.voxels,
+    }
+    save_voxels(voxels_path, voxels_data)
+
+    raw_points_path = os.path.join(settings.BASE_DIR, f'tmp/{uuid.uuid4()}.mat')
+    raw_points_data = {
+        'all': scan.detected_fiducials.fiducials,
+        'TP': scan.TP_B.fiducials,
+    }
+    scipy.io.savemat(raw_points_path, raw_points_data)
+
+    renderer = JSONRenderer()
+
+    phantom = serializers.PhantomSerializer(scan.phantom)
+    phantom_s = io.BytesIO()
+    phantom_s.write(renderer.render(phantom.data))
+
+    machine = serializers.MachineSerializer(scan.machine_sequence_pair.machine)
+    machine_s = io.BytesIO()
+    machine_s.write(renderer.render(machine.data))
+
+    sequence = serializers.SequenceSerializer(scan.machine_sequence_pair.sequence)
+    sequence_s = io.BytesIO()
+    sequence_s.write(renderer.render(sequence.data))
+
+    institution = serializers.InstitutionSerializer(scan.institution)
+    institution_s = io.BytesIO()
+    institution_s.write(renderer.render(institution.data))
+
+    files = {
+        'dicom.zip': scan.dicom_series.zipped_dicom_files.path,
+        'voxels.mat': voxels_path,
+        'raw_points.mat': raw_points_path,
+    }
+
+    streams = {
+        'phantom.json': phantom_s,
+        'machine.json': machine_s,
+        'sequence.json': sequence_s,
+        'institution.json': institution_s,
+    }
+
+    s = io.BytesIO()
+    with zipfile.ZipFile(s, 'w', zipfile.ZIP_DEFLATED) as zf:
+        for zip_path, path in files.items():
+            zf.write(path, zip_path)
+
+        for zip_path, stream in streams.items():
+            zf.writestr(zip_path, stream.getvalue())
+
+    return s
