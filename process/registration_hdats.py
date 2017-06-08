56249aff8606aa1a7f435aa5a53a9595a1dfa6f1
J. David Giese <johndgiese@gmail.com>
2017-06-08 02:41:32 -0400

    Include false positive in registration tests
    Also introduce a type of metric: FLE near isocenter

--- process/registration_hdats.py
+++ process/registration_hdats.py
@@ -41,6 +41,7 @@ class RegistrationSuite(Suite):
     3. Apply a random translation with a specified magnitude
     4. Rotate by specified amounts along each axis---specified in degrees
     5. Adds gaussian noise---the sigma for the noise is specified in mm
+    6. Add extra points
 
     The random seed for each test is also specified.
     '''
@@ -48,7 +49,7 @@ class RegistrationSuite(Suite):
 
     def collect(self):
         cases = {
-            '603A_max_trans': {
+            '603A_trans': {
                 'phantom': '603A',
                 'seed': 133,
                 'false_negative_fraction': 0,
@@ -56,8 +57,9 @@ class RegistrationSuite(Suite):
                 'rotations_in_degrees': (0, 0, 0),
                 'translation_magnitude_mm': 5,
                 'noise_sigma_mm': 0.3,
+                'false_positive_fraction': 0,
             },
-            '603A_max_trans_pruned': {
+            '603A_trans_pruned': {
                 'phantom': '603A',
                 'seed': 133,
                 'false_negative_fraction': 0.2,
@@ -65,8 +67,9 @@ class RegistrationSuite(Suite):
                 'rotations_in_degrees': (0, 0, 0),
                 'translation_magnitude_mm': 5,
                 'noise_sigma_mm': 0.3,
+                'false_positive_fraction': 0,
             },
-            '603A_max_rot': {
+            '603A_rot': {
                 'phantom': '603A',
                 'seed': 134,
                 'false_negative_fraction': 0,
@@ -74,8 +77,9 @@ class RegistrationSuite(Suite):
                 'rotations_in_degrees': (0, 0, 4),
                 'translation_magnitude_mm': 0.1,
                 'noise_sigma_mm': 0.3,
+                'false_positive_fraction': 0,
             },
-            '603A_max_rot_trans': {
+            '603A_rot_trans': {
                 'phantom': '603A',
                 'seed': 134,
                 'false_negative_fraction': 0,
@@ -83,6 +87,7 @@ class RegistrationSuite(Suite):
                 'rotations_in_degrees': (2, -2, 4),
                 'translation_magnitude_mm': 4,
                 'noise_sigma_mm': 0.3,
+                'false_positive_fraction': 0,
             },
             '603A_worstcase': {
                 'phantom': '603A',
@@ -92,6 +97,7 @@ class RegistrationSuite(Suite):
                 'rotations_in_degrees': (2, -2, 4),
                 'translation_magnitude_mm': 4,
                 'noise_sigma_mm': 0.1,
+                'false_positive_fraction': 1,
             },
         }
         return cases
@@ -102,7 +108,7 @@ class RegistrationSuite(Suite):
         dim, num_points = A.shape
         assert dim == 3
 
-        # 1. randomly remove points
+        # 1. randomly remove points (generate FNs)
         num_points_to_remove = round(case_input['false_negative_fraction']*num_points)
         points_to_remove = np.random.choice(num_points, num_points - num_points_to_remove, replace=False)
         B_pruned = A[:, points_to_remove]
@@ -123,7 +129,27 @@ class RegistrationSuite(Suite):
         # 5. add gaussian noise
         B_noise = B_translated_rotated + np.random.normal(0, case_input['noise_sigma_mm'], B_translated_rotated.shape)
 
-        return B_noise, xyztpx_expected
+        # 6. randomly add points (generate FPs)
+        extent_min = np.amin(B_noise, axis=1)
+        extent_max = np.amax(B_noise, axis=1)
+        extent_delta = extent_max - extent_min
+        num_points_to_add = round(case_input['false_positive_fraction']*num_points)
+        B_extra = np.zeros((3, num_points_to_add))
+        for d in [0, 1, 2]:
+            low = extent_min[d] - 0.2*extent_delta[d]
+            high = extent_max[d] + 0.2*extent_delta[d]
+            B_extra[d, :] = np.random.uniform(low, high, num_points_to_add)
+        B_final = np.hstack((B_noise, B_extra))
+
+        return B_final, xyztpx_expected
+
+    def FLE_percentiles_near_isocenter(self, TP_B, TP_A_S, isocenter_in_B):
+        distances_to_isocenter = np.linalg.norm(TP_B - isocenter_in_B.reshape(3, 1), axis=0)
+        distortion_free_radius = 30
+        points_near_isocenter = distances_to_isocenter < distortion_free_radius
+        TP_B_near_isocenter = TP_B[:, points_near_isocenter]
+        TP_A_S_near_isocenter = TP_A_S[:, points_near_isocenter]
+        return points_utils.FLE_percentiles(TP_A_S_near_isocenter, TP_B_near_isocenter)
 
     def run(self, case_input):
         metrics = OrderedDict()
@@ -137,10 +163,11 @@ class RegistrationSuite(Suite):
         context['B'] = B
         context['xyztpx_expected'] = xyztpx_expected
 
-        context['isocenter'] = np.array([0, 0, 0])
+        isocenter_in_B = xyztpx_expected[:3]
+        context['isocenter_in_B'] = isocenter_in_B
 
         xyztpx, FN_A_S, TP_A_S, TP_B, FP_B = rigidly_register_and_categorize(
-                A, B, context['isocenter'], skip_brute=True)
+                A, B, isocenter_in_B, skip_brute=True)
 
         context['xyztpx_actual'] = xyztpx
         x, y, z, theta, phi, xi = xyztpx
@@ -153,8 +180,12 @@ class RegistrationSuite(Suite):
         context['FP_B'] = FP_B
 
         FLE_percentiles = points_utils.FLE_percentiles(TP_A_S, TP_B)
-        for p in [0, 25, 50, 75, 95, 99, 100]:
+        FLE_percentiles_near_isocenter = self.FLE_percentiles_near_isocenter(
+                TP_A_S, TP_B, isocenter_in_B)
+
+        for p in [25, 50, 95, 99, 100]:
             metrics['FLE_{}'.format(p)] = FLE_percentiles[p]
+            metrics['FLE_{}_near_isocenter'.format(p)] = FLE_percentiles_near_isocenter[p]
 
         return metrics, context
 
@@ -185,16 +216,23 @@ class RegistrationSuite(Suite):
         print_xyztpx(context['xyztpx_actual'] - context['xyztpx_expected'])
         print('stats')
         print('TP = {}, FP = {}, FN = {}'.format(TP_B.shape[1], FP_B.shape[1], FN_A_S.shape[1]))
-        print(points_utils.format_FLE_percentile(metrics['FLE_99']))
+        print('FLE global')
+        print(points_utils.format_FLE_percentile(metrics['FLE_100']))
+        print(points_utils.format_FLE_percentile(metrics['FLE_95']))
         print(points_utils.format_FLE_percentile(metrics['FLE_50']))
         print(points_utils.format_FLE_percentile(metrics['FLE_25']))
+        print('FLE near isocenter')
+        print(points_utils.format_FLE_percentile(metrics['FLE_100_near_isocenter']))
+        print(points_utils.format_FLE_percentile(metrics['FLE_95_near_isocenter']))
+        print(points_utils.format_FLE_percentile(metrics['FLE_50_near_isocenter']))
+        print(points_utils.format_FLE_percentile(metrics['FLE_25_near_isocenter']))
 
         A_S = np.hstack((context['FN_A_S'], context['TP_A_S']))
 
         scatter3({
             'CAD': context['A'],
-            'CAD Shifted': A_S,
+            'CAD Registered to Detected': A_S,
             'Detected': context['B'],
-            'isocenter': context['isocenter'].reshape(3, 1),
+            'isocenter': context['isocenter_in_B'].reshape(3, 1),
         })
         plt.show()
