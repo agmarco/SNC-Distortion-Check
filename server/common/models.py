55de52f5dd44df21388954374ebe1c6337f0d0fc
J. David Giese <johndgiese@gmail.com>
2017-06-11 23:08:17 -0400

    Add note about blank serial numbers

--- server/common/models.py
+++ server/common/models.py
@@ -90,7 +90,9 @@ class PhantomModel(CommonFieldsMixin):
 class Phantom(CommonFieldsMixin):
     name_ht = 'This is how the phantom will be identified within the UI'
     name = models.CharField(max_length=255, help_text=name_ht)
-    institution = models.ForeignKey(Institution, models.CASCADE, null=True, blank=True)
+    institution = models.ForeignKey(Institution, models.CASCADE, null=True, blank=True, help_text=institution_ht)
+    institution_ht = 'The institution that owns this phantom.  If blank, new users can register an ' + \
+        'account to the phantom if they know the serial number.'
     model_ht = 'The model of phantom (e.g. the CIRS 603A head phantom)'
     model = models.ForeignKey(PhantomModel, models.CASCADE, help_text=model_ht)
     serial_number_ht = 'The Phantom\'s serial number'
