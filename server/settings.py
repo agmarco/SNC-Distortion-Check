c2953044b5aa9b42711746c79fae24f7a69e3c2d
J. David Giese <johndgiese@gmail.com>
2017-06-12 02:50:11 -0400

    Make S3 files private, and prevent over riding existing files
    The default S3 backend settings would allow new files with the same name
    to over-ride existing files.  This is bad, since our DICOM uploads are
    all placed in the same folder with the same name.  This would cause
    problems if people from different institutions uploaded zip archives
    with the same file name (which is pretty likely).
    
    The other setting prevents the backend from making all files
    public-read, which we don't want for security reasons.  I will have to
    test whether this breaks anything.

--- server/settings.py
+++ server/settings.py
@@ -187,6 +187,8 @@ else:
         raise ImproperlyConfigured('AWS env variable(s) missing')
     AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
     AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'private'
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
