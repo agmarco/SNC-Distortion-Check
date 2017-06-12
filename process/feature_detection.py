e6b07ce1316807a41efe398c137a1c63bbc60e17
J. David Giese <johndgiese@gmail.com>
2017-06-11 21:54:52 -0400

    Fix log statement

--- process/feature_detection.py
+++ process/feature_detection.py
@@ -40,7 +40,7 @@ class FeatureDetector:
         self.kernel = self._build_kernel()
         self.preprocessed_image = self._preprocess()
 
-        logger.info('convolving with gaussian kernel shape=%s, sigma=%.2f', self.kernel.shape, self.grid_spacing)
+        logger.info('convolving with gaussian kernel shape=%s, sigma=%.2f', self.kernel.shape, self.grid_radius)
         self.feature_image = signal.fftconvolve(self.preprocessed_image, self.kernel, mode='same')
 
         search_radius = self.grid_spacing/2.5
