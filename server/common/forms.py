6c9ad3b588af2190b88364af3c555b9301fb6de4
Zach Waggoner <zach.waggoner1@gmail.com>
2017-06-22 16:11:37 -0500

    Update form help_text for consistency

--- server/common/forms.py
+++ server/common/forms.py
@@ -290,7 +290,7 @@ class SequenceForm(CirsFormMixin, forms.ModelForm):
     tolerance = forms.FloatField(
         widget=forms.NumberInput(attrs={'step': '0.01'}),
         initial=Sequence._meta.get_field('tolerance').default,
-        help_text=Sequence.tolerance_ht,
+        help_text=Sequence._meta.get_field('tolerance').help_text,
     )
 
     class Meta:
