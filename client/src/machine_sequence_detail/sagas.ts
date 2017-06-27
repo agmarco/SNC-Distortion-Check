044b33aa9eaaae3db7e9189afa042a965adb493b
Zach Waggoner <zach.waggoner1@gmail.com>
2017-06-26 19:08:43 -0500

    Convert saga error handlers to function decorators

--- client/src/machine_sequence_detail/sagas.ts
+++ client/src/machine_sequence_detail/sagas.ts
@@ -2,6 +2,7 @@ import { delay } from 'redux-saga';
 import { call, put, all, select } from 'redux-saga/effects';
 
 import { IMachineSequencePairDto, IScanDto } from 'common/service';
+import { addOkCheck, addTimeout } from 'common/api';
 import * as actions from './actions';
 import * as selectors from './selectors';
 import Api from './api';
@@ -18,10 +19,10 @@ export function* pollScans(): any {
         } else {
             const unprocessedScans = scans.filter(s => s.processing);
             try {
-                const response = yield Api.pollScans({
+                const response = yield addOkCheck(addTimeout(Api.pollScans({
                     machine_sequence_pair_pk: MACHINE_SEQUENCE_PAIR.pk,
                     scan_pks: unprocessedScans.map(s => s.pk),
-                });
+                })));
                 const updatedScans = yield call(response.json.bind(response));
                 for (const scan of updatedScans) {
                     yield put(actions.updateScan(scan));
--- client/src/update_phantom/sagas.ts
+++ client/src/update_phantom/sagas.ts
@@ -3,6 +3,7 @@ import { delay } from 'redux-saga';
 import { call, put, all, select } from 'redux-saga/effects';
 
 import { IGoldenFiducialsDto, IPhantomDto } from 'common/service';
+import { addOkCheck, addTimeout } from 'common/api';
 import * as actions from './actions';
 import * as selectors from './selectors';
 import Api from './api';
@@ -20,10 +21,10 @@ function* pollCt(): any {
         } else {
             const unprocessedGoldenFiducialsSet = goldenFiducialsSet.filter(s => s.processing);
             try {
-                const response = yield Api.pollCt({
+                const response = yield addOkCheck(addTimeout(Api.pollCt({
                     phantom_pk: PHANTOM.pk,
                     golden_fiducials_pks: unprocessedGoldenFiducialsSet.map(g => g.pk),
-                });
+                })));
                 const updatedGoldenFiducialsSet = yield call(response.json.bind(response));
                 for (const goldenFiducials of updatedGoldenFiducialsSet) {
                     yield put(actions.updateGoldenFiducials(goldenFiducials));
