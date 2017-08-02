0b5d0d07f62a25626967a5300306ac61b2efa057
Reece Stevens <reecestevens24@gmail.com>
2017-08-02 16:09:28 -0500

    Adjust colorbar sizing

--- server/common/tasks.py
+++ server/common/tasks.py
@@ -399,12 +399,12 @@ def send_mail(subject_template_name, email_template_name,
 def add_colorbar(slices_array, units='mm'):
     max_val = np.round(np.max(slices_array))
     colorbar = np.zeros((100, 60))
-    gradient = np.linspace(max_val, 0, 90) * np.ones((20, 90))
-    colorbar[5:95, :20] = gradient.T
+    gradient = np.linspace(max_val, 0, 60) * np.ones((10, 60))
+    colorbar[5:65, :10] = gradient.T
     colorbar_img = Image.fromarray(colorbar)
     colorbar_canvas = ImageDraw.Draw(colorbar_img)
-    colorbar_canvas.text((20, 0), str(max_val)+units, fill=max_val)
-    colorbar_canvas.text((20, 85), "0"+units, fill=max_val)
+    colorbar_canvas.text((10, 0), str(max_val)+units, fill=max_val)
+    colorbar_canvas.text((10, 55), "0"+units, fill=max_val)
     colorbar = np.array(colorbar_img) * np.ones((len(slices_array), 100, 60))
     colorbar_area = slices_array[:, :100, :60]
     colorbar_area[colorbar != 0] = colorbar[colorbar != 0]
