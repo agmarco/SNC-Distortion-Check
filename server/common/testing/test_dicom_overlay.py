129ce659e94fec9585a636cd25706ed07f8dbeb3
Reece Stevens <reecestevens24@gmail.com>
2017-08-07 09:33:58 -0500

    Update test to reflect changes in colorbar

--- server/common/testing/test_dicom_overlay.py
+++ server/common/testing/test_dicom_overlay.py
@@ -1,20 +1,20 @@
-from copy import copy
-
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import imsave
 
 from ..overlay_utilities import add_colorbar
-
-COLORBAR_TOP_IDX = 5
-COLORBAR_BOTTOM_IDX = 64
-COLORBAR_WIDTH = 10
-COLORBAR_LENGTH = 60
+from ..overlay_utilities import (GRADIENT_WIDTH, GRADIENT_LENGTH,
+                                 GRADIENT_TOP_IDX, GRADIENT_BOTTOM_IDX)
 
 def test_dicom_overlay():
-    test_volume = np.zeros((512,512,700))
+    test_volume = np.zeros((700,512,512))
     max_value = 20
-    test_volume[300,300,300] = max_value
+    test_volume[:,300,300] = max_value
     voxels_with_colorbar = add_colorbar(test_volume)
-    new_colorbar_top = voxels_with_colorbar[:, COLORBAR_TOP_IDX, :COLORBAR_WIDTH]
-    new_colorbar_bottom = voxels_with_colorbar[:, COLORBAR_BOTTOM_IDX, :COLORBAR_WIDTH]
+    new_colorbar_top = voxels_with_colorbar[:, GRADIENT_TOP_IDX, 10:(GRADIENT_WIDTH+10)]
+    new_colorbar_bottom = voxels_with_colorbar[:, GRADIENT_BOTTOM_IDX, 10:(GRADIENT_WIDTH+10)]
     assert False not in np.equal(new_colorbar_top, max_value)
     assert False not in np.equal(new_colorbar_bottom, 0)
+
+def visualize_colorbar(slices_array):
+    imsave('example_array.png', slices_array[150,:,:])
