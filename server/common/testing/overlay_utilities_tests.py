e0f2752ce0002d300040f83e80a1bc9bd70aab87
J. David Giese <johndgiese@gmail.com>
2017-09-11 13:55:12 -0400

    Add tests for the zero extrapolated region

--- server/common/testing/overlay_utilities_tests.py
+++ server/common/testing/overlay_utilities_tests.py
@@ -1,9 +1,7 @@
+import pytest
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.misc import imsave
-from ..factories import DicomSeriesFactory
 
-from ..overlay_utilities import add_colorbar_to_slice
+from ..overlay_utilities import add_colorbar_to_slice, convex_hull_region
 from ..overlay_utilities import (GRADIENT_WIDTH, GRADIENT_LENGTH,
                                  GRADIENT_TOP_IDX, GRADIENT_BOTTOM_IDX)
 
@@ -21,6 +19,7 @@ def test_dicom_overlay():
     assert False not in np.equal(new_colorbar_top, expected_colorbar_top.T)
     assert False not in np.equal(new_colorbar_bottom, 0)
 
+
 def test_dicom_overlay_ticks_aligned():
     max_value = 20
     test_volume = np.ones((700,512,512))*(max_value/2)
@@ -32,13 +31,50 @@ def test_dicom_overlay_ticks_aligned():
     assert False not in np.equal(top_tick, max_value)
     assert False not in np.equal(bottom_tick, max_value)
 
+
 def test_dicom_overlay_not_added_when_slice_too_small():
     test_slice = np.random.rand(100, 140)
     original_slice = test_slice.copy()
     max_value = 20
     add_colorbar_to_slice(test_slice, max_value)
-
     assert np.array_equal(original_slice, test_slice)
 
-def visualize_colorbar(slices_array):
-    imsave('example_array.png', slices_array[192,:,:])
+
+@pytest.fixture
+def unit_cube():
+    return np.array([
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 0],
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 1],
+    ], dtype=np.float)
+
+
+def test_convex_hull_region_cube_in_corner(unit_cube):
+    grid_ranges = [[0, 2, 3j], [0, 2, 3j], [0, 2, 3j]]
+    expected_output = np.zeros((3, 3, 3), dtype=np.bool)
+    expected_output[:2, :2, :2] = True
+    actual_output = convex_hull_region(unit_cube, grid_ranges)
+    assert np.array_equal(expected_output, actual_output)
+
+
+def test_convex_hull_region_cube_in_center(unit_cube):
+    grid_ranges = [[-1, 2, 4j], [-1, 2, 4j], [-1, 2, 4j]]
+    expected_output = np.zeros((4, 4, 4), dtype=np.bool)
+    expected_output[1:3, 1:3, 1:3] = True
+    actual_output = convex_hull_region(unit_cube, grid_ranges)
+    assert np.array_equal(expected_output, actual_output)
+
+
+def test_convex_hull_region_cube_in_center_corner_removed(unit_cube):
+    grid_ranges = [[-1, 2, 4j], [-1, 2, 4j], [-1, 2, 4j]]
+    expected_output = np.zeros((4, 4, 4), dtype=np.bool)
+    expected_output[1:3, 1:3, 1:3] = True
+    expected_output[2, 2, 2] = False
+    unit_cube_wihout_111_corner = unit_cube[:-1]
+    actual_output = convex_hull_region(unit_cube_wihout_111_corner, grid_ranges)
+    assert np.array_equal(expected_output, actual_output)
