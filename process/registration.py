agmarco/SNC-Distortion-Check

commit 1231a895896b0c4ca092f2766951573b42aad5fb

Use a more lenient rho function during the initial convex optimization.
Before, if the initial guess had the detected points sufficiently far from the golden points, no points would be registered and the optimizer may find a local minimum that isn't aligned with the grid. This commit ensures that the initial guess is within a desired local minimum that will align the grids correctly.

process/registration.py
@@ -103,8 +103,8 @@ def rigidly_register(A, B_i, grid_spacing, xtol=registeration_tolerance):
     num_grid_spacings = 2
     g_cutoff = calculate_g_cutoff(num_grid_spacings, grid_spacing)
     g_near_isocenter = lambda bmag: 1 if bmag < g_cutoff else 0
-    rho = build_rho(g_cutoff, grid_spacing)
-    f_points_near_isocenter = build_objective_function(A, B_i, g_near_isocenter, rho)
+    rho_initial = build_rho(0, grid_spacing)
+    f_points_near_isocenter_initial = build_objective_function(A, B_i, g_near_isocenter, rho_initial)
 
     # During the grid search, we want to consider ALL points, regardless of how
     # far they are from the isocenter, since we want to ensure that A and B are
@@ -112,15 +112,14 @@ def rigidly_register(A, B_i, grid_spacing, xtol=registeration_tolerance):
     # earlier steps for performance reasons, and because we want to weight
     # points near the isocenter more during the fine-tuning optimization
     g_all = lambda bmag: 1.0
+    rho = build_rho(g_cutoff, grid_spacing)
     f_all_points = build_objective_function(A, B_i, g_all, rho)
+    f_points_near_isocenter = build_objective_function(A, B_i, g_near_isocenter, rho)
 
     xyztpx_initial = np.array([0, 0, 0, 0, 0, 0])
-    xyztpx_initial_local_minimum = _run_optimizer(f_points_near_isocenter, xyztpx_initial, xtol)
+    xyztpx_initial_local_minimum = _run_optimizer(f_points_near_isocenter_initial, xyztpx_initial, xtol)
     xyztpx_global_minimum_rough = grid_search(f_all_points, grid_spacing, xyztpx_initial_local_minimum)
-    if np.array_equal(xyztpx_initial_local_minimum, xyztpx_global_minimum_rough):
-        xyztpx_global_minimum = xyztpx_global_minimum_rough
-    else:
-        xyztpx_global_minimum = _run_optimizer(f_points_near_isocenter, xyztpx_global_minimum_rough, xtol)
+    xyztpx_global_minimum = _run_optimizer(f_points_near_isocenter, xyztpx_global_minimum_rough, xtol)
     logger.info('finished rigid registration')
     return xyztpx_global_minimum
 

commit b59a046c1a10cedaf611346ccc9d3cbc02df208e

Fix failing registration test
This failure was introduced by commit
1231a895896b0c4ca092f2766951573b42aad5fb

This commit used a more lenient rho function for our first pass on the
registration, however, the first pass rho function is built using a g_cutoff of 0,
which causes the `build_rho` function to always return the
`max_match_distance`.  Since this test had a `grid_spacing` of 1, the
`max_match_distance` was negative!  By changing the `overlap_buffer`
such that it is tied to the grid_spacing, we avoid this sitution.

process/registration.py
@@ -141,8 +141,7 @@ def build_rho(g_cutoff, grid_spacing):
     thus may be further apart while still being considered "matched".
     '''
     min_match_distance = 3
-    overlap_buffer = 1
-    max_match_distance = grid_spacing / 2 - overlap_buffer
+    max_match_distance = grid_spacing * 0.45
 
     def rho(bmag):
         if bmag < g_cutoff: