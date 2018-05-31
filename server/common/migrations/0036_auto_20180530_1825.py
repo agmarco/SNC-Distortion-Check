8638ec5051326358b3fc70e08f231286b480a89d
Zach Waggoner <zach.waggoner1@gmail.com>
2018-05-31 00:57:09 -0500

    Skip over bad dicom files (non-zip) in migration

--- server/common/migrations/0036_auto_20180530_1825.py
+++ server/common/migrations/0036_auto_20180530_1825.py
@@ -12,17 +12,20 @@ from process import dicom_import
 def populate_dicom_fields(apps, schema_editor):
     DicomSeries = apps.get_model('common', 'DicomSeries')
     for dicom_series in DicomSeries.objects.all():
-        with zipfile.ZipFile(dicom_series.zipped_dicom_files, 'r') as f:
-            datasets = dicom_import.dicom_datasets_from_zip(f)
-        first_dataset = datasets[0]
-        dicom_series.patient_name = getattr(first_dataset, 'PatientName', None)
-        dicom_series.patient_birth_date = getattr(first_dataset, 'PatientBirthDate', None)
-        dicom_series.patient_sex = getattr(first_dataset, 'PatientSex', None)
-        dicom_series.modality = getattr(first_dataset, 'Modality', None)
-        dicom_series.rows = getattr(first_dataset, 'Rows', None)
-        dicom_series.columns = getattr(first_dataset, 'Columns', None)
-        dicom_series.number_of_slices = getattr(first_dataset, 'NumberOfSlices', len(datasets))
-        dicom_series.save()
+        try:
+            with zipfile.ZipFile(dicom_series.zipped_dicom_files, 'r') as f:
+                datasets = dicom_import.dicom_datasets_from_zip(f)
+            first_dataset = datasets[0]
+            dicom_series.patient_name = getattr(first_dataset, 'PatientName', None)
+            dicom_series.patient_birth_date = getattr(first_dataset, 'PatientBirthDate', None)
+            dicom_series.patient_sex = getattr(first_dataset, 'PatientSex', None)
+            dicom_series.modality = getattr(first_dataset, 'Modality', None)
+            dicom_series.rows = getattr(first_dataset, 'Rows', None)
+            dicom_series.columns = getattr(first_dataset, 'Columns', None)
+            dicom_series.number_of_slices = getattr(first_dataset, 'NumberOfSlices', len(datasets))
+            dicom_series.save()
+        except zipfile.BadZipFile:
+            pass
 
 
 class Migration(migrations.Migration):
