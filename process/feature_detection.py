5cd8f9e5b677007b9138149fbd1c4fe287f58f33
J. David Giese <johndgiese@gmail.com>
2018-07-01 14:44:55 -0400

    Improve variable names

--- process/feature_detection.py
+++ process/feature_detection.py
@@ -29,25 +29,23 @@ class FeatureDetector:
         self.ijk_to_xyz = ijk_to_xyz
         self.voxel_spacing = affine.voxel_spacing(self.ijk_to_xyz)
 
-        r_mm = 3.5  # NOTE: this should probably be proportional to the grid_radius
-        sigma_mm = modality_sigmas_mm[self.modality]
-
         self.grid_spacing = phantoms.paramaters[phantom_model]['grid_spacing']
-
         self.preprocessed_image = self._preprocess_image(image)
 
-        sigmas = sigma_mm / self.voxel_spacing
-        logger.info('blurring with gaussian kernel, sigma=%.2f %s', sigma_mm, str(sigmas))
-        self.feature_image = gaussian_filter(self.preprocessed_image, sigmas, mode='reflect')
+        sigma_mm = modality_sigmas_mm[self.modality]
+        sigma_px = sigma_mm / self.voxel_spacing
+        logger.info('blurring with gaussian kernel, sigma=%.2f %s', sigma_mm, str(sigma_px))
+        self.feature_image = gaussian_filter(self.preprocessed_image, sigma_px, mode='reflect')
 
         if limit_memory_usage: del self.preprocessed_image
 
-        search_radius = self.grid_spacing/4
+        search_radius_mm = self.grid_spacing/4
+        center_of_mass_radius_mm = 3.5  # should probably be proportional to grid_radius
         self.points_ijk, self.label_image = peak_detection.detect_peaks(
             self.feature_image,
             self.voxel_spacing,
-            search_radius,
-            r_mm,
+            search_radius_mm,
+            center_of_mass_radius_mm,
         )
         if limit_memory_usage: del self.label_image
         if limit_memory_usage: del self.feature_image
