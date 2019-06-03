7f53f81bfba7269e98c178e074d5500cbf2d1a82
Zach Waggoner <zach.waggoner1@gmail.com>
2019-06-03 18:51:02 -0500

    Revert "Fix namespace warning"
    This reverts commit 2f95121034fb109b198aaed911aa6a36952449e0.

--- client/src/machine_sequence_detail/app.tsx
+++ client/src/machine_sequence_detail/app.tsx
@@ -2,9 +2,11 @@ import { renderApp } from 'common/utils';
 import { IScanDto, IMachineSequencePairDto } from 'common/service';
 import App from './containers/App';
 
-interface Window {
-    MACHINE_SEQUENCE_PAIR: IMachineSequencePairDto;
-    SCANS: IScanDto[];
+declare global {
+    interface Window {
+        MACHINE_SEQUENCE_PAIR: IMachineSequencePairDto;
+        SCANS: IScanDto[];
+    }
 }
 
 renderApp(App, 'machine-sequence-detail-app');
