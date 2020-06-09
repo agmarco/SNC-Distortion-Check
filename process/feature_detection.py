00d2cf0b56d6bffa3fce73dca98f7a81a6c78293
J. David Giese <johndgiese@gmail.com>
2020-06-09 15:30:15 -0400

    Make the blur distance depend on phantom model

--- process/feature_detection.py
+++ process/feature_detection.py
@@ -14,8 +14,14 @@ logger = logging.getLogger(__name__)
 
 
 modality_sigmas_mm = {
-    'mri': 1.65,
-    'ct': 1.2,
+    ('mri', '603A'): 1.65,
+    ('mri', '603-GS'): 1.65,
+    ('mri', '604'): 1.65,
+    ('mri', '604-GS'): 1.65,
+    ('ct', '603A'): 1.2,
+    ('ct', '603-GS'): 1.6,  # due to spheres at intersections
+    ('ct', '604'): 1.2,
+    ('ct', '604-GS'): 1.2,  # TODO: see if this should be larger
 }
 
 
@@ -32,7 +38,7 @@ class FeatureDetector:
         self.grid_spacing = phantoms.paramaters[phantom_model]['grid_spacing']
         self.preprocessed_image = self._preprocess_image(image)
 
-        sigma_mm = modality_sigmas_mm[self.modality]
+        sigma_mm = modality_sigmas_mm[(self.modality, self.phantom_model)]
         sigma_px = sigma_mm / self.voxel_spacing
         logger.info('blurring with gaussian kernel, sigma=%.2f %s', sigma_mm, str(sigma_px))
         self.feature_image = gaussian_filter(self.preprocessed_image, sigma_px, mode='reflect')
