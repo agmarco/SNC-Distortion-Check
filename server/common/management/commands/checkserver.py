9397afb309ec02c37bca3fff82bbb1052816c8c1
Emma Curtis <ecurtis@innolitics.com>
2020-11-19 22:59:30 -0800

    Add heroku CLI buildpack

--- server/common/management/commands/checkserver.py
+++ server/common/management/commands/checkserver.py
@@ -1,12 +1,12 @@
-# from django.core.management.base import BaseCommand
-# from server.common.worker_utilities import worker_is_on, stop_worker, no_jobs_in_queue
-#
-#
-# class Command(BaseCommand):
-#     help = 'Manage worker state'
-#
-#     def handle(self, **options):
-#         if worker_is_on() and no_jobs_in_queue():
-#             stop_worker()
-#         else:
-#             pass
+from django.core.management.base import BaseCommand
+from server.common.worker_utilities import worker_is_on, stop_worker, no_jobs_in_queue
+
+
+class Command(BaseCommand):
+    help = 'Manage worker state'
+
+    def handle(self, **options):
+        if worker_is_on() and no_jobs_in_queue():
+            stop_worker()
+        else:
+            pass
--- server/common/worker_utilities.py
+++ server/common/worker_utilities.py
@@ -1,37 +1,37 @@
-# import json
-# import subprocess
-# from server.celery import app
-#
-#
-# def worker_is_on():
-#     bash_command = "heroku ps --app cirs-dev worker --json"
-#     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-#     output, error = process.communicate()
-#     if output:
-#         worker_response = json.loads(output.decode('utf-8'))
-#         return worker_response[0].get('state') == 'up' or 'starting'
-#     else:
-#         return False
-#
-#
-# def no_jobs_in_queue():
-#     celery_info = app.control.inspect()
-#     worker_status = celery_info.app.current_worker_task
-#     if not worker_status:
-#         return True
-#     else:
-#         return False
-#
-#
-# def start_worker():
-#     if not worker_is_on():
-#         bash_command = "heroku ps:scale --app cirs-dev worker=1:performance-l"
-#         subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-#     return
-#
-#
-# def stop_worker():
-#     if worker_is_on():
-#         bash_command = "heroku ps:scale --app cirs-dev worker=0:performance-l"
-#         subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
-#     return
+import json
+import subprocess
+from server.celery import app
+
+
+def worker_is_on():
+    bash_command = "heroku ps --app cirs-dev worker --json"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    if output:
+        worker_response = json.loads(output.decode('utf-8'))
+        return worker_response[0].get('state') == 'up' or 'starting'
+    else:
+        return False
+
+
+def no_jobs_in_queue():
+    celery_info = app.control.inspect()
+    worker_status = celery_info.app.current_worker_task
+    if not worker_status:
+        return True
+    else:
+        return False
+
+
+def start_worker():
+    if not worker_is_on():
+        bash_command = "heroku ps:scale --app cirs-dev worker=1:performance-l"
+        subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    return
+
+
+def stop_worker():
+    if worker_is_on():
+        bash_command = "heroku ps:scale --app cirs-dev worker=0:performance-l"
+        subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    return
