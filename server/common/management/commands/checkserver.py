ccefe1e66ca23877d2e6d77f44458a32bf08cbf4
Emma Curtis <ecurtis@innolitics.com>
2020-11-20 00:36:20 -0800

    Add return statement to checkserver command

--- server/common/management/commands/checkserver.py
+++ server/common/management/commands/checkserver.py
@@ -7,6 +7,6 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         if worker_is_on() and no_jobs_in_queue():
-            stop_worker()
+            return stop_worker()
         else:
             print('worker server is not on')
