4b2871d3345d036636a986f89437f7c2606769a2
Emma Curtis <ecurtis@innolitics.com>
2020-11-23 11:31:34 -0800

    Write better error messages in worker utilities

--- server/common/worker_utilities.py
+++ server/common/worker_utilities.py
@@ -21,8 +21,8 @@ def worker_is_on():
     except Exception:
         if heroku_app_name:
             logger.debug("""{0} was thrown when checking dynos on {1} app in Heroku. Check to make sure the app name 
-            matches an app in the cirs heroku dashboard and the dynos are configured""".format(
-                           Exception, heroku_app_name))
+            matches an app in the cirs heroku dashboard.""".format(
+                Exception, heroku_app_name))
         else:
             logger.debug("""{0} was thrown because you have not set HEROKU_APP_NAME in your .env file. To 
             resolve this error, set the key to 'cirs-dev' or 'cirs-production'.""".format(Exception))
@@ -34,15 +34,23 @@ def no_jobs_in_queue():
         celery_info = app.control.inspect()
         return not celery_info.active() or celery_info.scheduled() or celery_info.registered()
     except CeleryError:
-        logger.debug('Celery queue not checked', CeleryError)
+        logger.debug('The celery queue was not checked because it threw this exception: {0}'.format(CeleryError))
         return False
 
 
 def start_worker():
     if not worker_is_on():
-        return heroku_app.process_formation()['worker'].scale(1)
+        try:
+            return heroku_app.process_formation()['worker'].scale(1)
+        except Exception:
+            logger.debug(
+                '{0} was thrown when trying to scale up the worker dyno on {1}.'.format(Exception, heroku_app_name))
 
 
 def stop_worker():
-    if worker_is_on():
-        return heroku_app.process_formation()['worker'].scale(0)
+    try:
+        if worker_is_on():
+            return heroku_app.process_formation()['worker'].scale(0)
+    except Exception:
+        logger.debug(
+            '{0} was thrown when trying to scale down the worker dyno on {1}.'.format(Exception, heroku_app_name))
