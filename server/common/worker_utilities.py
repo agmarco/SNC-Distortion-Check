6919e25b8e7f8c02474f82abaaffb68578f2dd12
Emma Curtis <ecurtis@innolitics.com>
2020-11-23 07:04:55 -0800

    Add exception clauses to worker_is_on and no_jobs_in_queue

--- server/common/worker_utilities.py
+++ server/common/worker_utilities.py
@@ -1,26 +1,38 @@
 import os
+import warnings
 
+from celery.exceptions import CeleryError
 import heroku3
 
 from server.celery import app
 
-
 heroku_connection = heroku3.from_key(os.getenv('HEROKU_API_KEY'))
-heroku_app = heroku_connection.apps()[os.getenv('HEROKU_APP_NAME')]
-celery_info = app.control.inspect()
+heroku_app_name = os.getenv('HEROKU_APP_NAME')
+heroku_app = heroku_connection.apps()[heroku_app_name]
 
 
 def worker_is_on():
-    if heroku_app.dynos():
+    try:
         active_dynos = [dyno.type for dyno in heroku_app.dynos()]
         return 'worker' in active_dynos
-    else:
+    except Exception:
+        if os.getenv('HEROKU_APP_NAME'):
+            warnings.warn("""{0} was thrown when checking dynos on {1} app in Heroku. Check to make sure the app name 
+            is matches an app in the cirs heroku dashboard, and the dynos are configured""".format(
+                           Exception, heroku_app_name))
+        else:
+            warnings.warn("""{0} was thrown because you have not set HEROKU_APP_NAME in your .env file. To 
+            resolve this error, set the key to 'cirs-dev' or 'cirs-production'.""".format(Exception))
         return False
 
 
-
 def no_jobs_in_queue():
-    return not celery_info.active() or celery_info.scheduled() or celery_info.registered()
+    try:
+        celery_info = app.control.inspect()
+        return not celery_info.active() or celery_info.scheduled() or celery_info.registered()
+    except CeleryError:
+        warnings.warn('Celery queue not checked', CeleryError)
+        return False
 
 
 def start_worker():
