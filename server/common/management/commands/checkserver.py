4801c7eca9b2e50f57c375e0e8c4b09ab0b724a8
Emma Curtis <ecurtis@innolitics.com>
2020-11-23 11:12:32 -0800

    Change warnings.warn to logger messages

--- server/common/management/commands/checkserver.py
+++ server/common/management/commands/checkserver.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.core.management.base import BaseCommand
+
 from server.common.worker_utilities import worker_is_on, stop_worker, no_jobs_in_queue
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -9,6 +14,6 @@ class Command(BaseCommand):
         if worker_is_on() and no_jobs_in_queue():
             return stop_worker()
         elif not no_jobs_in_queue():
-            print('Jobs it queue. Keeping worker server on.')
+            logger.info('Jobs in queue. Keeping worker server on.')
         else:
-            print('Worker is already off.')
+            logger.info('Worker is already off.')
--- server/common/worker_utilities.py
+++ server/common/worker_utilities.py
@@ -1,5 +1,5 @@
+import logging
 import os
-import warnings
 
 from celery.exceptions import CeleryError
 import heroku3
@@ -10,6 +10,8 @@ heroku_connection = heroku3.from_key(os.getenv('HEROKU_API_KEY'))
 heroku_app_name = os.getenv('HEROKU_APP_NAME')
 heroku_app = heroku_connection.apps()[heroku_app_name]
 
+logger = logging.getLogger(__name__)
+
 
 # Base exception thrown because heroku3 exceptions are not defined in documentation
 def worker_is_on():
@@ -18,11 +20,11 @@ def worker_is_on():
         return 'worker' in active_dynos
     except Exception:
         if heroku_app_name:
-            warnings.warn("""{0} was thrown when checking dynos on {1} app in Heroku. Check to make sure the app name 
+            logger.debug("""{0} was thrown when checking dynos on {1} app in Heroku. Check to make sure the app name 
             matches an app in the cirs heroku dashboard and the dynos are configured""".format(
                            Exception, heroku_app_name))
         else:
-            warnings.warn("""{0} was thrown because you have not set HEROKU_APP_NAME in your .env file. To 
+            logger.debug("""{0} was thrown because you have not set HEROKU_APP_NAME in your .env file. To 
             resolve this error, set the key to 'cirs-dev' or 'cirs-production'.""".format(Exception))
         return False
 
@@ -32,7 +34,7 @@ def no_jobs_in_queue():
         celery_info = app.control.inspect()
         return not celery_info.active() or celery_info.scheduled() or celery_info.registered()
     except CeleryError:
-        warnings.warn('Celery queue not checked', CeleryError)
+        logger.debug('Celery queue not checked', CeleryError)
         return False
 
 
