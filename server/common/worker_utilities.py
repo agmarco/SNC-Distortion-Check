02cc6c6430d4719532fb197b09c166d29326760c
Emma Curtis <ecurtis@innolitics.com>
2020-11-30 10:11:37 -0800

    Create class for connecting to Heroku3

--- server/common/worker_utilities.py
+++ server/common/worker_utilities.py
@@ -10,22 +10,26 @@ from server.celery import app
 logger = logging.getLogger(__name__)
 
 
-# Base exception thrown because heroku3 exceptions are not defined in documentation
+class HerokuConnect:
+    def __init__(self):
+        self.heroku_connection = heroku3.from_key(os.getenv('HEROKU_API_KEY'))
+        self.heroku_app_name = os.getenv('APP_NAME')
+        self.heroku_app = self.heroku_connection.apps()[self.heroku_app_name]
+
+
 def worker_is_on():
     try:
-        heroku_connection = heroku3.from_key(os.getenv('HEROKU_API_KEY'))
-        heroku_app_name = os.getenv('APP_NAME')
-        heroku_app = heroku_connection.apps()[heroku_app_name]
-        active_dynos = [dyno.type for dyno in heroku_app.dynos()]
+        heroku_connection = HerokuConnect()
+        active_dynos = [dyno.type for dyno in heroku_connection.heroku_app.dynos()]
         return 'worker' in active_dynos
     except Exception:
-        if heroku_app_name:
+        if heroku_connection.heroku_app_name:
             logger.error("""{0} was thrown when checking dynos on {1} app in Heroku. Check to make sure the app name 
             matches an app in the cirs heroku dashboard.""".format(
-                Exception, heroku_app_name))
+                Exception, heroku_connection.heroku_app_name))
         else:
             logger.debug("""{0} was thrown because you have not set APP_NAME in your .env file. To 
-            resolve this error, set the key to 'cirs-dev' or 'cirs-production'.""".format(Exception))
+            resolve this error, set the key to 'cirs-dev' or 'cirs-production'.""".format(type(Exception).__name__))
         return False
 
 
@@ -41,22 +45,18 @@ def no_jobs_in_queue():
 def start_worker():
     if not worker_is_on():
         try:
-            heroku_connection = heroku3.from_key(os.getenv('HEROKU_API_KEY'))
-            heroku_app_name = os.getenv('APP_NAME')
-            heroku_app = heroku_connection.apps()[heroku_app_name]
-            return heroku_app.process_formation()['worker'].scale(1)
+            heroku_connection = HerokuConnect()
+            return heroku_connection.heroku_app.process_formation()['worker'].scale(1)
         except Exception:
             logger.error(
-                '{0} was thrown when trying to scale up the worker dyno on {1}.'.format(Exception, heroku_app_name))
+                '{0} was thrown when trying to scale up the worker dyno on {1}.'.format(type(Exception).__name__, heroku_connection.heroku_app_name))
 
 
-def stop_worker():
+def stop_worker(self):
     try:
-        if worker_is_on():
-            heroku_connection = heroku3.from_key(os.getenv('HEROKU_API_KEY'))
-            heroku_app_name = os.getenv('APP_NAME')
-            heroku_app = heroku_connection.apps()[heroku_app_name]
-            return heroku_app.process_formation()['worker'].scale(0)
+        if self.worker_is_on():
+            heroku_connection = HerokuConnect()
+            return heroku_connection.heroku_app.process_formation()['worker'].scale(0)
     except Exception:
         logger.error(
-            '{0} was thrown when trying to scale down the worker dyno on {1}.'.format(Exception, heroku_app_name))
+            '{0} was thrown when trying to scale down the worker dyno on {1}.'.format(type(Exception).__name__, heroku_connection.heroku_app_name))
