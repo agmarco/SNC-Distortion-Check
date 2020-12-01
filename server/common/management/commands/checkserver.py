327515676e8e5613a27d5059ed287ad3e7921520
Emma Curtis <ecurtis@innolitics.com>
2020-11-30 18:38:05 -0800

    Refacter heroku worker server methods to class and show exeptions on the browser

--- server/common/management/commands/checkserver.py
+++ server/common/management/commands/checkserver.py
@@ -2,7 +2,8 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from server.common.worker_utilities import worker_is_on, stop_worker, no_jobs_in_queue
+from server.common.heroku_api import HerokuConnect
+from server.celery import app
 
 logger = logging.getLogger(__name__)
 
@@ -10,10 +11,18 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'Manage worker state'
 
+    def _no_jobs_in_queue(self):
+        try:
+            celery_info = app.control.inspect()
+            return not celery_info.active() or celery_info.scheduled() or celery_info.registered()
+        except Exception:
+            logging.error('Celery app not detected.')
+
     def handle(self, **options):
-        if worker_is_on() and no_jobs_in_queue():
-            return stop_worker()
-        elif not no_jobs_in_queue():
+        heroku_connection = HerokuConnect()
+        if heroku_connection.worker_is_on() and self._no_jobs_in_queue():
+            return heroku_connection.stop_worker()
+        elif not self._no_jobs_in_queue():
             logger.info('Jobs in queue. Keeping worker server on.')
         else:
             logger.info('Worker is already off.')
