ec26ef68d6a4b7413ea6015e290d9d006a072e9a
Emma Curtis <ecurtis@innolitics.com>
2020-12-01 07:50:24 -0800

    Create method to force worker server to turn on before returning view

--- server/common/management/commands/checkserver.py
+++ server/common/management/commands/checkserver.py
@@ -1,8 +1,9 @@
 import logging
 
+from celery.exceptions import CeleryError
 from django.core.management.base import BaseCommand
 
-from server.common.heroku_api import HerokuConnect
+from server.common.heroku_api import HerokuAPI
 from server.celery import app
 
 logger = logging.getLogger(__name__)
@@ -11,18 +12,20 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'Manage worker state'
 
-    def _no_jobs_in_queue(self):
+    @staticmethod
+    def _no_jobs_in_queue():
         try:
             celery_info = app.control.inspect()
             return not celery_info.active() or celery_info.scheduled() or celery_info.registered()
-        except Exception:
+        except CeleryError:
             logging.error('Celery app not detected.')
 
     def handle(self, **options):
-        heroku_connection = HerokuConnect()
+        heroku_connection = HerokuAPI()
         if heroku_connection.worker_is_on() and self._no_jobs_in_queue():
             return heroku_connection.stop_worker()
         elif not self._no_jobs_in_queue():
             logger.info('Jobs in queue. Keeping worker server on.')
         else:
             logger.info('Worker is already off.')
+
