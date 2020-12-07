3820a6299688f32d3a05e2e7c24c4c936e6b2377
ejcurtis <53452158+ejcurtis@users.noreply.github.com>
2020-12-07 18:08:27 -0500

    Remove check for registered tasks

--- server/common/management/commands/checkserver.py
+++ server/common/management/commands/checkserver.py
@@ -13,25 +13,27 @@ class Command(BaseCommand):
     help = 'Manage worker state'
 
     @staticmethod
-    def _no_jobs_in_queue():
+    def jobs_in_queue():
         try:
-            celery_info = app.control.inspect()
-            if celery_info.active() or celery_info.scheduled() or celery_info.registered():
-                logger.info(f"""There are {celery_info.scheduled()} celery tasks scheduled; {celery_info.active()} 
-                tasks active and {celery_info.registered()} tasks registered.""")
-                return False
-            else:
+            celery_inspect = app.control.inspect()
+            celery_info = {
+                'scheduled': len(list(chain(*celery_inspect.scheduled().values()))),
+                'active': len(list(chain(*celery_inspect.scheduled().values())))
+            }
+            if celery_info['active'] or celery_info['scheduled']:
+                logger.info(f'There are {celery_info["scheduled"]} celery tasks scheduled and {celery_info["scheduled"]} tasks active.')
                 return True
+            else:
+                return False
         except CeleryError:
             logger.error('Celery app not detected.')
 
     def handle(self, **options):
         heroku_connection = HerokuAPI()
-        if heroku_connection.worker_is_on() and self._no_jobs_in_queue():
+        if heroku_connection.worker_is_on() and not self.jobs_in_queue():
             heroku_connection.stop_worker()
             return logger.info('Worker server scaled to 0.')
-        elif not self._no_jobs_in_queue():
+        elif self.jobs_in_queue():
             logger.info('Jobs in queue. Keeping worker server on.')
         else:
             logger.info('Worker is already off.')
-
