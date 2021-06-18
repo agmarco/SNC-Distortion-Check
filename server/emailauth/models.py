543af20054313a382d2be486e3aa99a21395cbae
tmckeage <tmckeage@cirsinc.com>
2021-06-18 16:08:56 -0400

    Revert "Update models.py"
    This reverts commit 6c251beda6094e5204d53d2c6a8f3144f77c1d27.

--- server/emailauth/models.py
+++ server/emailauth/models.py
@@ -12,8 +12,8 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a User with the given email and password.
         """
-        #if not email:
-        #    raise ValueError('The given email must be set')
+        if not email:
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
